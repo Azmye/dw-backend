e5d1e854ffeb9bb3c7784c463e27ed91486e205b
unknown <mhmmdazmye@gmail.com>
2023-05-02 02:31:46 +0700

    fix eps

--- dto/episode/episode_request.go
+++ dto/episode/episode_request.go
@@ -7,7 +7,7 @@ type EpisodeRequest struct {
 	Title         string      `json:"title" form:"title" gorm:"type: varchar(255)"`
 	ThumbnailFilm string      `json:"thumbnail" form:"thumbnail" gorm:"type: varchar(255)"`
 	Year          int         `json:"year" form:"year"`
-	LinkFilm      string      `json:"linkFilm" form:"link" gorm:"type: varchar(255)"`
+	LinkFilm      string      `json:"linkFilm" form:"link" gorm:"type: varchar(255)" validate:"required"`
 	FilmID        int         `json:"film_id" `
 	Film          models.Film `json:"film"`
 }
